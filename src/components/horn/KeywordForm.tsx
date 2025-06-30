390cc933b5dc40d0adb113c6082dec73003fc021
SeongYoonMin <namo4862@naver.com>
2025-07-01 00:50:10 +0900

    styles: keywordform 스타일 수정
    shadcn ui 로 변경

--- src/components/horn/KeywordForm.tsx
+++ src/components/horn/KeywordForm.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const KeywordForm = ({
   addKeyword,
@@ -18,13 +20,14 @@ const KeywordForm = ({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
+    <form onSubmit={handleSubmit} className="w-full flex max-w-[400px] gap-2">
+      <Input
         type="text"
         onChange={handleKeyword}
+        className="py-1 px-2 flex-1 border rounded-md"
         placeholder="등록하고자 하는 키워드를 입력해주세요."
       />
-      <button type="submit">추가</button>
+      <Button type="submit" variant={"outline"} className="cursor-pointer">추가</Button>
     </form>
   );
 };
