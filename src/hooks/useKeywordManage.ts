bb4ea6c25caed5dbd75c2ac6bc3365d1a00c8f2a
SeongYoon <namo4862@gmail.com>
2025-07-04 16:04:59 +0900

    feat: keywordList Custom Hooks 구현

--- src/hooks/useKeywordManage.ts
+++ src/hooks/useKeywordManage.ts
@@ -1,1 +1,26 @@
-export const useKeywordManage = () => {}
+import { useStore } from "zustand";
+import { keywordStore } from "@/stores/keywordStore";
+
+export const useKeywordManage = () => {
+  const { keywordList, addKeyword, removeKeyword, resetKeywordList } =
+    useStore(keywordStore);
+
+  const handleAddKeyword = (keyword: string) => {
+    addKeyword(keyword);
+  };
+
+  const handleRemoveKeyword = (keyword: string) => {
+    removeKeyword(keyword);
+  };
+
+  const handleResetKeywordList = () => {
+    resetKeywordList();
+  };
+
+  return {
+    keywordList,
+    handleAddKeyword,
+    handleRemoveKeyword,
+    handleResetKeywordList,
+  };
+};
