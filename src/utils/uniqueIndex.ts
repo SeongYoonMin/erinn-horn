a05cb513a5d9ce1bc86cf5c565400b640f9a52a0
SeongYoon <namo4862@gmail.com>
2025-07-04 16:06:11 +0900

    feat: uniqueIndex 공용 함수 구현

---
+++ src/utils/uniqueIndex.ts
@@ -0,0 +1,10 @@
+export const uniqueIndex = (index: number) => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  return `${year}-${month}-${day}-${hour}-${minute}-${second}-${index}`;
+};
