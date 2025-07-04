fee85eabe15870ec68f63057590624fc1b8ad130
SeongYoon <namo4862@gmail.com>
2025-07-04 16:00:50 +0900

    feat: 테이블기반 뿔피리 구현

--- src/components/horn/index.ts
+++ src/components/horn/index.ts
@@ -1,4 +1,5 @@
-export { default as HornList } from "./HornList";
-export { default as HornItems } from "./HornItems";
 export { default as KeywordForm } from "./KeywordForm";
 export { default as ServerForm } from "./ServerForm";
+export { HornTable } from "./HornTable";
+export { default as HornList } from "./HornList";
+export { columns } from "./columns";
